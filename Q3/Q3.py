061e5a540696ec1e8f703d8ad2c14932a7ec41cf
samil06 <samildincer@gmail.com>
2023-05-28 15:55:52 +0300

    q3 is done

--- Q3/Q3.py
+++ Q3/Q3.py
@@ -1,1 +1,16 @@
 
+def fun():
+    inp = input('Write string:  ')
+    lowercase_letter_number=0
+    uppercase_letter_number=0
+    for i in inp:
+        letter_count=i.isupper()
+        if letter_count==1:
+            uppercase_letter_number=uppercase_letter_number+1
+        else:
+            lowercase_letter_number=lowercase_letter_number+1
+    print(inp)
+    return lowercase_letter_number,uppercase_letter_number
+
+print(fun())
+
