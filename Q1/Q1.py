3bb3ab9ad11cdbf409d29d54ec4c6b156dce0f91
samil06 <samildincer@gmail.com>
2023-05-28 14:23:42 +0300

    q1 is done

--- Q1/Q1.py
+++ Q1/Q1.py
@@ -1,10 +1,10 @@
-def sort_tuples(tuples_list):
-    # Sort the list of tuples based on the second element of each tuple
-    sorted_tuples = sorted(tuples_list, key=lambda x: x[1])
-    return sorted_tuples
+
+def takeSecond(elem):
+    return elem[1]
 
 # List of tuples
 tuples_list = [(1, 2), (3, 1), (4, 5), (6, 0), (7, 4)]
-
+tuples_list.sort(key=takeSecond)
 # Call the function and print the result
-print(sort_tuples(tuples_list))
+print(tuples_list)
+
